aaa9b09943f318678ca9eb30681ed032c9c8249d
nohabrafi <charliebbarkin@tutanota.com>
2022-03-16 17:03:16 +0100

    second commit

--- partials/header.php
+++ partials/header.php
@@ -13,10 +13,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <style>
         body {
-            /* background: -webkit-gradient(linear, left top, left bottom, from(#ffbb99), to(#4d79ff)) fixed; */
-
-            background: linear-gradient(to top, #ffbb99, #4d79ff);
-            
+            background: linear-gradient(to top, #4d79ff, #ffbb99);
             min-height: 100vh;
         }
     </style>
